3ff4d80c1cd0bcfb664a28e7a08155602cc47c22
Mathewshin <144893000+Mathewshin@users.noreply.github.com>
2023-09-25 12:56:23 +0530

    Challenge unit -2

--- main (2).py
+++ main (2).py
@@ -1,24 +1,18 @@
-# 1.1 implement a recursive function to calculate the factorial of a given number.
-"""
-1! = 1 * 1
-2! = 2 * 1! --->2 * 1
-3! = 3 * 2! --->3 * 2 * 1
-.
-.
-10! = 10 * 9! ---> 10 * 9 * 8 *... * 1
-
-Formula - n * (n-1)!
-"""
-
-
-def fact_rec(n):
-  if n == 0 or n == 1:
-    return 1
-  else:
-    return n * fact_rec(n - 1)
-
-
-number = int(input("enter a value :"))
-res = fact_rec(number)
-
-print("the factorial of {} is {}.".format(number, res))
+# Define the Player class
+class Player:
+    def play(self):
+        print("The player is playing cricket.")
+# Define the Batsman class, derived from Player
+class Batsman(Player):
+    def play(self):
+        print("The batsman is batting.")
+# Define the Bowler class, derived from Player
+class Bowler(Player):
+    def play(self):
+        print("The bowler is bowling.")
+# Create objects of Batsman and Bowler classes
+batsman = Batsman()
+bowler = Bowler()
+# Call the play() method for each object
+batsman.play()
+bowler.play()
